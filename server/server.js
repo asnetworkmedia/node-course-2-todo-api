93a51669ccbc704ed9111971739cc1e1d2a15c99
Hamid Shahrokhi <Hamid Shahrokhi>
2016-11-02 02:15:13 -0400

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('../db/mongoose');
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 
 var app = new express();
 
@@ -22,8 +21,8 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-
-
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 })
+
+module.exports = {app};
