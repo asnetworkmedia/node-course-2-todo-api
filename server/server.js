03d831766f11b85066a1780ef90332d180aeabd9
Hamid Shahrokhi <Hamid Shahrokhi>
2016-11-03 13:57:00 -0400

    Add GET /users/me and authentication middlewear

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middlewear/authenticate');
 
 var app = new express();
 var port = process.env.PORT;
@@ -52,7 +53,6 @@ app.get('/todos/:id', (req, res) => {
 
 	}).catch((e) => { res.status(400).send() });
 });
-
 
 app.delete('/todos/:id', (req, res) => {
 	var id = req.params.id;
@@ -109,6 +109,11 @@ app.post('/users/', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
